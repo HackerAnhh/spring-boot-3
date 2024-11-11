2d4b4a5ec77beb4ddc69d6de687362f9b21feea2
HackerAnhh <122959369+HackerAnhh@users.noreply.github.com>
2024-11-11 14:45:01 +0700

    Update s.bat

--- hello-spring-boot/s.bat
+++ hello-spring-boot/s.bat
@@ -1,5 +1,2 @@
-@ehco off
 @REM browser-sync start --proxy "localhost:8080" --port 9090 --files "src/main/resources/**/*.*" "src/test/**/*.*"
 browser-sync start --proxy "localhost:8080" --port 9090 --files "src/**/*.*" 
-
-
