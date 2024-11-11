8a48f4b729e8a8a6a1584d4381e6d6c8eeb7c1d2
HackerAnhh <122959369+HackerAnhh@users.noreply.github.com>
2024-11-11 14:52:15 +0700

    Delete hello-spring-boot/s.bat

--- hello-spring-boot/s.bat
+++
@@ -1,3 +0,0 @@
-@REM browser-sync start --proxy "localhost:8080" --port 9090 --files "src/main/resources/**/*.*" "src/test/**/*.*"
-browser-sync start --proxy "localhost:8080" --port 9090 --files "src/**/*.*" 
-
